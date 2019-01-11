398fb834b94ddf3e4f17f2ccc8d20f1e4595ce55
loader94 <chimer88@gmail.com>
2019-01-11 17:07:11 +0100

    Test

---
+++ testgita/testgita/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace testgita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            for (int i = 0; i < 100; i++)
+                Console.Write(a.Add(i));
+        }
+    }
+}
